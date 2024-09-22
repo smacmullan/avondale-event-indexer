c692a19de12ba04b1c55d9db2cc7e03b1a21fc7a
Sean MacMullan <sean.macmullan@gmail.com>
2024-09-22 18:26:15 -0500

    Update event list print function to write to file instead of the console.

--- src/utils/printEventList.js
+++ src/utils/printEventList.js
@@ -1,23 +1,32 @@
+import fs from 'fs';
 import { formatTimeRange, formatDay, eventSort } from './time.js';
 
-export function printEventList(events) {
+export function printEventList(events, filePath="output/eventList.txt") {
     // Sort events by start date/time
     events.sort(eventSort);
 
     let currentDay = '';
+    let textOutput = '';
     events.forEach(event => {
         const eventDay = formatDay(event);
 
         // Print the day only once per group of events
         if (eventDay !== currentDay) {
             currentDay = eventDay;
-            console.log(`\n\n${eventDay}`);
+            textOutput += `\n\n${eventDay}\n`;
         }
 
         const organizationName = event.organizer.name || "Unknown";
 
         // Format the event summary
         const timeRange = formatTimeRange(event);
-        console.log(`* ${timeRange} - ${event.name} | ${organizationName}`);
+        textOutput += `* ${timeRange} - ${event.name} | ${organizationName}\n`;
     });
+
+    try {
+        fs.writeFileSync(filePath, textOutput);
+        console.log(`Event list saved to "${filePath}"`);
+    } catch (err) {
+        console.error('Error writing event list text output to file', err);
+    }
 }
